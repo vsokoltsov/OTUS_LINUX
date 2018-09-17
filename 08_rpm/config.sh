66fb9c1e8817d1e1b91ad6958a5fd965cbcbdda2
Vadim Sokoltsov <vforvad@gmail.com>
2018-09-18 01:01:45 +0300

    add rpm repo homework

---
+++ 08_rpm/config.sh
@@ -0,0 +1,9 @@
+sudo yum update -y
+sudo yum install -y epel-release
+sudo yum install -y nginx createrepo
+sudo systemctl start nginx
+cd /usr/share/nginx/html
+sudo rm -rf *
+sudo cp /home/vagrant/rpm_lab/ip2w-0.0.1-1.noarch.rpm ./
+sudo createrepo /usr/share/nginx/html
+sudo createrepo --update /usr/share/nginx/html
